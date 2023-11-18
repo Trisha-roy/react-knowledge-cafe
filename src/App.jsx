b92ac00b248e03ed3627342ac06f27e1b905b5bb
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-11-17 20:13:37 -0400

    Added Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       
-      <h1 className='text-5xl bg-red-400 text-center'>Knowledge-Cafe</h1>
+      <Header></Header>
       
      
     </>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-5xl font-bold bg-red-400 text-center'>Knowledge-Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
