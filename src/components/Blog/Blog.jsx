c0fcfc55af56accd6242afae3177f8ff7c9213e1
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-11-21 17:10:20 -0400

    Added blog title

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,55 @@
 import PropTypes from 'prop-types'; 
+import Icon from '../../assets/images/Frame.svg'
 
 const Blog = ({blog}) => {
 
-    const {title,cover}=blog
+    const {title,cover,author,author_img,read_times,hashtags,posted_date}=blog
+
+
     return (
         <div>
+
+
+
             <img src={cover}></img>
+
+
+
+            <div className='flex justify-between'>
+
+                <div className='flex'>
+                <img className='w-14 h-14 rounded-3xl' src={author_img}></img>
+
+                <div>
+                    <h2 className='text-2xl'>{author}</h2>
+                    <p>{posted_date}</p>
+                </div>
+
+                </div>
+
+
+                <div className='flex'>
+                <span>{read_times} min read </span>
+
+
+                <div>
+                <img src={Icon}></img>
+                </div>
+
+                
+                </div>
+            </div>
+
+
             <h4 className='text-4xl'>{title}</h4>
+            <p>
+
+                {
+                    hashtags.map((hashtag,index)=><span key={index}> <a href=''> #{hashtag} </a> </span>)
+                }
+            </p>
+
+
         </div>
     );
 };
