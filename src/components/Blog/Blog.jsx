d18b273b8e984a193ce118485b94a79f5592c2de
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-11-21 01:05:46 -0400

    Added Blog Cover Photos

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,9 +1,12 @@
 import PropTypes from 'prop-types'; 
 
 const Blog = ({blog}) => {
+
+    const {title,cover}=blog
     return (
         <div>
-            
+            <img src={cover}></img>
+            <h4 className='text-4xl'>{title}</h4>
         </div>
     );
 };
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
     const [blogs,setBlogs]=useState([])
@@ -16,6 +17,11 @@ const Blogs = () => {
 
 
             <h1 className="text-4xl">Blogs : {blogs.length}</h1>
+            {
+
+                blogs.map(blog=><Blog  blog={blog}></Blog>)
+
+            }
 
         </div>
     );
